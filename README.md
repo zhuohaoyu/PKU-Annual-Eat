47361d472afc5a119a908215fe03ea2b02dc0cf5
leverimmy <leverimmy@gmail.com>
2024-12-21 04:30:00 +0800

    feat: Update codes.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # THU-Annual-Eat
+
 一年过去了，你在华子食堂里花的钱都花在哪儿了？
+
+## 项目简介
+
+本项目是一个用于统计华清大学学生在食堂（和宿舍）的消费情况的脚本。通过模拟登录清华大学校园卡网站，获取学生在华子食堂的消费记录，并通过数据可视化的方式展示。
+
+![demo](./demo.png)
+
+## 使用方法
+
+### 0. 获取服务代码
+
+首先，登录校园卡账号后，在[华清大学校园卡网站](https://card.tsinghua.edu.cn/userselftrade)获取你的服务代码。
+
+![card](./card.png)
+
+`F12` 打开开发者工具，切换到 Network（网络）标签页，然后 `Ctrl + R` 刷新页面，找到 `userselftrade` 这个请求，查看标头中的 `Cookie` 字段，其中包含了你的服务代码。
+
+服务代码就是 `servicehall=` 后面的一串字符，复制下来，后面会用到。
+
+![servicehall](./servicehall.png)
+
+### 1. 安装依赖
+
+本项目依赖于 `requests`、`matplotlib` 和 `pycryptodome`，请确保你的 Python 环境中已经安装了这些库。
+
+```bash
+pip install requests matplotlib pycryptodome
+```
+
+> 你可能需要在 `Python\Python312\Lib\site-packages` 目录下将 `crypto` 文件夹改名为 `Crypto`。
+
+### 2. 运行脚本
+
+```bash
+python main.py
+```
+
+首次运行时，请输入你的账号、密码、学号和“服务代码”，会自动保存在 `config.json` 文件中。
+
+### 3.（可选）修改配置
+
+如果你想修改账号、密码、学号或者服务代码，可以直接修改 `config.json` 文件。
+
+```json
+{
+    "username": "你的账号",
+    "password": "你的密码",
+    "idserial": "你的学号",
+    "servicehall": "你的服务代码"
+}
+```
+
+## LICENSE
+
+除非另有说明，本仓库的内容采用 [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/) 许可协议。在遵守许可协议的前提下，您可以自由地分享、修改本文档的内容，但不得用于商业目的。
+
+如果您认为文档的部分内容侵犯了您的合法权益，请联系项目维护者，我们会尽快删除相关内容。
---
+++ main.py
@@ -0,0 +1,83 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+import base64
+import json
+import matplotlib.pyplot as plt
+import requests
+
+def decrypt_aes_ecb(encrypted_data: str) -> str:
+    
+    key = encrypted_data[:16].encode('utf-8')
+    encrypted_data = encrypted_data[16:]
+    encrypted_data_bytes = base64.b64decode(encrypted_data)
+    
+    cipher = AES.new(key, AES.MODE_ECB)
+    
+    decrypted_data = unpad(cipher.decrypt(encrypted_data_bytes), AES.block_size)
+
+    return decrypted_data.decode('utf-8')
+
+username = ""
+password = ""
+idserial = ""
+servicehall = ""
+all_data = dict()
+
+if __name__ == "__main__":
+    # 读入账户信息
+    try:
+        with open("config.json", "r", encoding='utf-8') as f:
+            account = json.load(f)
+            username = account["username"]
+            password = account["password"]
+            idserial = account["idserial"]
+            servicehall = account["servicehall"]
+    except Exception as e:
+        print("账户信息读取失败，请重新输入")
+        username = input("请输入用户名: ")
+        password = input("请输入密码: ")
+        idserial = input("请输入学号: ")
+        servicehall = input("请输入服务代码: ")
+        with open("account.json", "w", encoding='utf-8') as f:
+            json.dump({"username": username, "password": password, "idserial": idserial, "servicehall": servicehall}, f, indent=4)
+    
+    # 发送请求，得到加密后的字符串
+    url = f"https://card.tsinghua.edu.cn/business/querySelfTradeList?pageNumber=0&pageSize=5000&starttime=2024-01-01&endtime=2024-12-31&idserial={idserial}&tradetype=-1"
+    cookie = {
+        "servicehall": servicehall,
+    }
+    response = requests.post(url, cookies=cookie)
+
+    # 解密字符串
+    encrypted_string = json.loads(response.text)["data"]
+    decrypted_string = decrypt_aes_ecb(encrypted_string)
+
+    # 整理数据
+    data = json.loads(decrypted_string)
+    for item in data["resultData"]["rows"]:
+        try:
+            if item["mername"] in all_data:
+                all_data[item["mername"]] += item["txamt"]
+            else:
+                all_data[item["mername"]] = item["txamt"]
+        except Exception as e:
+            pass
+    all_data = {k: round(v / 100, 2) for k, v in all_data.items()} # 将分转换为元，并保留两位小数
+    print(len(all_data))
+    # 输出结果
+    all_data = dict(sorted(all_data.items(), key=lambda x: x[1], reverse=False))
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.figure(figsize=(12, len(all_data) / 66 * 18))
+    plt.barh(list(all_data.keys()), list(all_data.values()))
+    for index, value in enumerate(list(all_data.values())):
+        plt.text(value + 0.01 * max(all_data.values()),
+                index,
+                str(value),
+                va='center')
+        
+    # plt.tight_layout()
+    plt.xlim(0, 1.2 * max(all_data.values()))
+    plt.title("华清大学食堂消费情况")
+    plt.xlabel("消费金额（元）")
+    plt.savefig("result.png")
+    plt.show()
